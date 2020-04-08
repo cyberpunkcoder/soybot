4b2a61a283fb15cf3f561dd99e24e1b84047e9dd
cyberpunkprogrammer <cyberpunkprogrammer@gmail.com>
2020-04-08 16:46:16 -0700

    Added kick, ban and hello commands
    Added the kick command, which admins and helpers can use.
    Added the ban command, which admins can use.
    Added the hello command, which anyone can use.
    Made sure permissions checking works.

--- events/message.js
+++ events/message.js
@@ -1,7 +1,33 @@
+// Import the command files
+const hello = require("../commands/hello");
 const kick = require("../commands/kick");
+const ban = require("../commands/ban");
 
+// Name of admin role
+const admin = 'Administrator';
+
+// Name of helper role
+const helper = 'Helper';
+
+// Handle any commands
 module.exports = (client, message) => {
-	if (message.content.startsWith("!kick")) {
-		return kick(message);
+	if (message.content.startsWith("!hello")) {
+		return hello(message);
+	} 
+	else if (message.content.startsWith("!kick")) {
+		if (message.member.roles.cache.some(role => role.name === admin || role.name == helper)) {
+			return kick(message);
+		} 
+		else {
+			return message.reply(`You need to be an ${admin} or ${helper} to use this command!`);
+		}
+	}
+	else if (message.content.startsWith("!ban")) {
+		if (message.member.roles.cache.some(role => role.name === admin)) {
+			return ban(message);
+		} 
+		else {
+			return message.reply(`You need to be an ${admin} to use this command!`);
+		}
 	}
 };
